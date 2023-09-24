1c823f08dee322b7f3f13c7b0d03828cd2a06821
h0ttab <red19650226@yandex.ru>
2023-09-25 01:28:26 +0300

    Almost done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const body = document.querySelector('.quizBody');
+
 const question__counter = document.querySelector(".question__counter");
 const question__text = document.querySelector(".question__text");
 
@@ -34,10 +36,11 @@ class Question {
     }
 }
 
-const questions = [new Question('Who are you1?', 'Me1', "Me2", "Me3", "Me4", "Me2"), new Question('Who are you?', 'Me111', "Me222", "Me333", "Me444", "Me22")]
+const questions = [new Question('Столица США?', 'Вашингтон', "Сиэттл", "Нью-Йорк", "Даллас", "Вашингтон"), new Question('Как звали Пушкина?', 'Алексей', "Александр", "Андрей", "Анатолий", "Александр")]
 
 function answerOnClick(option){
     nextButton.className = "quizBody__nextButton";
+    nextButton.removeAttribute('disabled');
     nextButton.addEventListener('click', nextQuestion)
     const button = document.querySelector(`.${option}>input[type="radio"]`);
     const answers = document.querySelectorAll(".quizBody__answerOption")
@@ -69,12 +72,16 @@ function renderPage(){
         answerOption3.textContent = question["option3"];
         answerOption4.textContent = question["option4"];
         if (question.orderNumber == questions.length) {
-            nextButton.textContent = 'Review my answers'
-            nextButton.removeEventListener('click', nextQuestion)
+            nextButton.textContent = 'Перейти к результатам'
             nextButton.addEventListener('click', getScore)
+            nextButton.disabled  = 'disabled'
         }
     } else {
-        return
+        nextButton.className = "quizBody__nextButton-disabled";
+        const toHide = document.querySelectorAll('.quizBody>*')
+        toHide.forEach((el)=>{
+            el.style.display = 'none';
+        })
     }
 }
 
@@ -95,9 +102,22 @@ function nextQuestion(){
 }
 
 function getScore(){
+     let corretAnswersTotal = 0;
     questions.forEach((el)=>{
-        //
+        if(el.userAnswer == el.correctAnswer){
+            corretAnswersTotal += 1;
+        }
     })
+    const resultPage = document.createElement('div');
+    resultPage.className = 'resultPage';
+    
+    const resultText = document.createElement('span');
+    resultText.className = 'resultText'; 
+    resultText.innerText = `Правильных ответов : 
+    ${corretAnswersTotal} из ${questions.length}`;
+    
+    resultPage.appendChild(resultText);
+    body.appendChild(resultPage);
 }
 
 renderPage();
